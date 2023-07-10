3307410d4155ac8059806668b835074b85012a93
Lord_Doug <106497954+DougVikt@users.noreply.github.com>
2023-07-09 23:11:36 -0300

    edit navbar

--- s-fitness/src/Elementos/Navbar/menu-lateral/menu_lateral.js
+++ s-fitness/src/Elementos/Navbar/menu-lateral/menu_lateral.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-  
 
-function ElementosMenu({nome ,caminho ,imagem}){
+function TopMenu({imagem}){
+  return(
+    <a href="index.html" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref={imagem}></use></svg>
+      <span className="fs-4">Barra Lateral</span>
+    </a>
+                  
+  );
+}
+
+function ElementosLinha({nome ,caminho ,imagem}){
     return(
     <li className="nav-item">
-        <a href={caminho} className="nav-link active">
+        <a href={caminho} className="nav-link link-dark">
           <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref={imagem}></use></svg>
           {nome}
         </a>
@@ -13,19 +22,32 @@ function ElementosMenu({nome ,caminho ,imagem}){
     );
 }
 
-export default function MenuLateral(){
-    return(
-    <ul className="navbar-nav">
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-            <svg className="bi pe-none me-2" width="40" height="32"></svg>
-            <span className="fs-4"><font style={{verticalAlign:'inherit'}}><font style={{verticalAlign: 'inherit'}}>Menu</font></font></span>
-        </a>  
+
+function ElementosMenu(){
+  return(
+     <ul className="navbar-nav">
+        <TopMenu /> 
+        <hr></hr>
         <ul className="nav nav-pills flex-column mb-auto">
-          <ElementosMenu nome={'inicio'}  caminho={'/s-fitness/public/index.html'} imagem={"http://www.w3.org/2000/svg"}/>
-          <ElementosMenu nome={'teste'}  caminho={'/s-fitness/public/index.html'} imagem={"http://www.w3.org/2000/svg"}/>
+          <ElementosLinha nome={'inicio'}  caminho={'/s-fitness/public/index.html'} imagem={"http://www.w3.org/2000/svg"}/>
+          <ElementosLinha nome={'teste'}  caminho={'/s-fitness/public/index.html'} imagem={"http://www.w3.org/2000/svg"}/>
 
         </ul>    
         <hr></hr>
     </ul>
+  );
+}
+
+
+export default function MenuLateral(){
+    return(
+    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div className="offcanvas-body">
+          <ElementosMenu />
+      </div>
+    </div> 
     );
 }
--- s-fitness/src/Elementos/Navbar/navbar.js
+++ s-fitness/src/Elementos/Navbar/navbar.js
@@ -3,30 +3,29 @@ import React from "react";
 import MenuLateral from "./menu-lateral/menu_lateral";
 
 
-
+function BottonLogin(){
+  return(
+    <div className="">
+      <a className="btn btn-warning" href="#" role="button">
+        LOGIN
+      </a> 
+    </div>
+    
+  );
+}
 
 export default function Navbar() {
   return (
     <nav className="navbar bg-light fixed-top">
       <div className="container-fluid">
-        <a className="navbar-brand w-75 justify-content-top" href="index.html">
+        <a className="navbar-brand w-50" href="index.html">
           S-Fitness
         </a>
-        <div classNameName="btn-menu">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar"
-            aria-controls="offcanvasNavbar"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
-        <div className="offcanvas-body">
-          <MenuLateral />
-        </div>
-        
+        <BottonLogin />
+        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <MenuLateral />
       </div>
     </nav>
   );
